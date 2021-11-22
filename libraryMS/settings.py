11a7234dd227c31fabb27db3ad46bbc791bb93ed
ksoftdev <kuntursoftdev@gmail.com>
2021-11-22 12:56:51 -0500

    Deploy 009

--- libraryMS/settings.py
+++ libraryMS/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'libraryMS.wsgi.application'
 
 HEROKU = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
